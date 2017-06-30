621d4310f3cbdd6eabbda0c425f82d7b0a5ecc93
ohanssen <ohanssen@acm.org>
2017-06-30 21:33:25 +0200

    Update example

--- mapconfig.js
+++ mapconfig.js
@@ -88,14 +88,14 @@
                cacheSize: 4096
                
             })
-          }) 
+          })
 
   ]);
   
 
 /* Overlay for all projections */
 LAYERS ( 
-   { base:false, 
+   { base: false, 
      predicate: function() { return scale() < 800; }
    },
    [  
@@ -115,6 +115,8 @@ LAYERS (
                params: {'LAYERS':'UTMrutenett', VERSION: "1.1.1"},
             }) 
       })
+
+      
    ]);
 
 
@@ -126,20 +128,15 @@ LAYERS (
  */
 var defaultView = 'default';
 VIEWS ([
-   { name: 'finnm',     title: 'Finnmark',        extent: [18.3575, 68.26,   32.4980, 71.8444] },
-   { name: 'ntroms',    title: 'Nord-Troms',      extent: [16.7582, 68.8402, 23.4135, 70.4735] },
-   { name: 'tromso',    title: 'Tromsø',          extent: [18.5793, 69.5524, 19.4027, 69.7525] },
-   { name: 'mtroms',    title: 'Midt/sør-Troms',  extent: [15.1248, 68.2508, 21.4869, 69.8232] },
-   { name: 'ofoten',    title: 'Ofoten/Lofoten',  extent: [12.1127, 67.7717, 18.3217, 69.239]  },
-   { name: 'salten',    title: 'Salten',          extent: [11.3947, 66.5,    17.2634, 67.9832] },
-   { name: 'helg',      title: 'Helgeland',       extent: [10.0569, 65.1156, 15.5983, 66.6494] },  
+   { name: 'finnm',     title: 'Finnmark',        extent: [19.006, 68.064, 33.848, 71.662] },
+   { name: 'troms',     title: 'Troms',           extent: [15.549, 68.641, 22.454, 70.196]  },
+   { name: 'nordl',     title: 'Nordland',        extent: [8.242, 65.462, 20.91, 68.301] },
    { name: 'ntrond',    title: 'Nord-Trøndelag',  extent: [ 9.0436, 63.2859, 15.3995, 64.8541] },
    { name: 'strond',    title: 'Sør-Trøndelag',   extent: [ 7.3903, 62.0338, 13.5351, 63.6724] },
    { name: 'moreroms',  title: 'Møre og Romsdal', extent: [ 3.5993, 61.5234,  9.7916, 63.3084] },
    { name: 'sognf',     title: 'Sogn og fjordane',extent: [ 2.8448, 60.4411,  8.8474, 62.2549] },
    { name: 'hordal',    title: 'Hordaland',       extent: [ 3.1295, 59.3777,  8.9257, 61.1814] },
-   { name: 'rogal',     title: 'Rogaland',        extent: [ 3.3536, 58.0768,  8.9212, 59.8724] },
-   { name: 'agder',     title: 'Agder',           extent: [ 4.7145, 57.7071, 10.1809, 59.4536] },
+   { name: 'agrog',     title: 'Agder/Rogaland',  extent: [4.508,57.965,9.311,59.645]},
    { name: 'tele',      title: 'Telemark',        extent: [ 7.4777, 58.7404, 10.2363, 59.5893] },
    { name: 'hardanger', title: 'Hardangervidda/Buskerud', extent: [ 6.4033, 59.3222, 12.0772, 61.0049] },
    { name: 'oslofj',    title: 'Østfold/Vestfold',extent: [ 7.8612, 58.8272, 13.3971, 60.4553] },
