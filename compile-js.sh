ac1f179995f3194a335d00becbc26204e16272bf
ohanssen <ohanssen@acm.org>
2017-11-25 13:23:27 +0100

    Order of files in compilation

--- compile-js.sh
+++ compile-js.sh
@@ -5,12 +5,12 @@
 #
 
 cd mapbrowser
-closure-compiler --js jscoord.js --js config.js --js mapref.js --js mapbrowser.js --js widget.js --js uiSupport.js --js popup.js --js popupmenu.js --js measure.js --js toolbar.js --js areaList.js --js mousepos.js --js layerSwitcher.js --js refSearch.js --js configSupport.js > ../mapbrowser-min.js
+closure-compiler --js config.js --js server.js --js jscoord.js --js mapref.js --js mapbrowser.js --js widget.js --js uiSupport.js --js popup.js --js popupmenu.js --js measure.js --js toolbar.js --js areaList.js --js mousepos.js --js layerSwitcher.js --js refSearch.js --js configSupport.js > ../mapbrowser-min.js
 
 cd ..
 cd layeredit
-closure-compiler --js layerList.js --js layerEdit.js --js wmsLayer.js --js wfsLayer.js > ../layeredit-min.js
+closure-compiler --js layerEdit.js --js layerList.js --js wmsLayer.js --js wfsLayer.js > ../layeredit-min.js
 
 cd ..
 cd tracking
-closure-compiler --js mapupdate.js --js tracking.js --js search.js --js filters.js > ../tracking-min.js
+closure-compiler --js tracking.js --js polaricserver.js --js mapupdate.js --js search.js --js filters.js > ../tracking-min.js
