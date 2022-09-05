344ee7befc5431eb414de520425c76aad6d87409
Øyvind Hanssen <ohanssen@acm.org>
2022-09-05 20:15:15 +0200

    Support ES8 (2017)

--- compile-js.sh
+++ compile-js.sh
@@ -15,24 +15,24 @@ closure_compiler () {
 #
 
 D=mapbrowser
-closure_compiler --language_in ECMASCRIPT_2015 --js $D/config.js --js $D/server.js --js $D/jscoord.js --js $D/mapref.js --js $D/mapbrowser.js --js $D/widget.js --js $D/featureinfo.js --js $D/uiSupport.js --js $D/popup.js --js $D/popupmenu.js --js $D/measure.js --js $D/toolbar.js --js $D/areaList.js --js $D/mousepos.js --js $D/layerSwitcher.js --js $D/refSearch.js --js $D/configSupport.js --js $D/docreader.js $D/mapInfo.js > mapbrowser-min.js
+closure_compiler --language_in ECMASCRIPT_2017 --js $D/config.js --js $D/server.js --js $D/jscoord.js --js $D/mapref.js --js $D/mapbrowser.js --js $D/widget.js --js $D/featureinfo.js --js $D/uiSupport.js --js $D/popup.js --js $D/popupmenu.js --js $D/measure.js --js $D/toolbar.js --js $D/areaList.js --js $D/mousepos.js --js $D/layerSwitcher.js --js $D/refSearch.js --js $D/configSupport.js --js $D/docreader.js $D/mapInfo.js > mapbrowser-min.js
 
 D=layeredit
-closure_compiler --language_in ECMASCRIPT_2015 --js $D/layerEdit.js --js $D/layerList.js --js $D/wmsLayer.js --js $D/wfsLayer.js --js $D/gpxLayer.js --js $D/drawingLayer.js > layeredit-min.js
+closure_compiler --language_in ECMASCRIPT_2017 --js $D/layerEdit.js --js $D/layerList.js --js $D/wmsLayer.js --js $D/wfsLayer.js --js $D/gpxLayer.js --js $D/drawingLayer.js > layeredit-min.js
 
 D=tracking
-closure_compiler --language_in ECMASCRIPT_2015 --js $D/tracking.js --js $D/polaricserver.js --js $D/pubsub.js --js $D/mapupdate.js --js $D/search.js --js $D/filters.js --js $D/notifier.js --js $D/bullboard.js --js $D/history.js --js $D/heardvia.js --js $D/labelStyle.js --js $D/trackeralias.js --js $D/globalsettings.js --js $D/mytrackers.js --js $D/ownobjects.js --js $D/signs.js --js $D/ownpos.js --js $D/sarmode.js --js $D/trailinfo.js --js $D/tags.js  --js $D/users.js --js $D/passwd.js --js $D/bikewheel.js --js $D/mailbox.js --js $D/sharing.js --js $D/aprspackets.js --js $D/pointinfo.js --js $D/telemetry.js --js $D/telhist.js > tracking-min.js
+closure_compiler --language_in ECMASCRIPT_2017 --js $D/tracking.js --js $D/polaricserver.js --js $D/pubsub.js --js $D/mapupdate.js --js $D/search.js --js $D/filters.js --js $D/notifier.js --js $D/bullboard.js --js $D/history.js --js $D/heardvia.js --js $D/labelStyle.js --js $D/trackeralias.js --js $D/globalsettings.js --js $D/mytrackers.js --js $D/ownobjects.js --js $D/signs.js --js $D/ownpos.js --js $D/sarmode.js --js $D/trailinfo.js --js $D/tags.js  --js $D/users.js --js $D/passwd.js --js $D/bikewheel.js --js $D/mailbox.js --js $D/sharing.js --js $D/aprspackets.js --js $D/pointinfo.js --js $D/telemetry.js --js $D/telhist.js > tracking-min.js
 
 D=featureedit
-closure_compiler --language_in ECMASCRIPT_2015 --js $D/snow/drawConfig.js --js $D/snow/drawGlobals.js --js $D/snow/undoFunctions.js --js $D/snow/drawStyle.js --js $D/snow/drawFunctions.js --js $D/snow/drawTooltipHelper.js --js $D/snow/drawEvents.js --js $D/snow/gpxDownload.js --js $D/snow/drawIcons.js --js $D/snow/mithrilDrawBox.js --js $D/snow/mithrilIcons.js --js $D/snow/measureTooltip.js --js $D/featureEdit.js --js $D/properties.js > featureedit-min.js
+closure_compiler --language_in ECMASCRIPT_2017 --js $D/snow/drawConfig.js --js $D/snow/drawGlobals.js --js $D/snow/undoFunctions.js --js $D/snow/drawStyle.js --js $D/snow/drawFunctions.js --js $D/snow/drawTooltipHelper.js --js $D/snow/drawEvents.js --js $D/snow/gpxDownload.js --js $D/snow/drawIcons.js --js $D/snow/mithrilDrawBox.js --js $D/snow/mithrilIcons.js --js $D/snow/measureTooltip.js --js $D/featureEdit.js --js $D/properties.js > featureedit-min.js
 
 
-closure_compiler --language_in ECMASCRIPT_2015 --js application.js > application-min.js
+closure_compiler --language_in ECMASCRIPT_2017 --js application.js > application-min.js
 
 #
 # combine and minify css
 #
 cd style
-cat polaric.css popup.css tracking.css mobil.css drawStyle.css mobileStyle.css | cleancss -o style-min.css
+cat polaric.css widget.css popup.css tracking.css mobil.css drawStyle.css mobileStyle.css | cleancss -o style-min.css
 
 cd ..
