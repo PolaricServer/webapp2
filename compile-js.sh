59cfb9e46efcc9df0d3da9b8ca4ef8ee2bcc03bf
Øyvind Hanssen <ohanssen@acm.org>
2023-09-27 20:49:32 +0200

    Update compile script

--- compile-js.sh
+++ compile-js.sh
@@ -8,7 +8,7 @@ BABEL="babeljs --minified --compact=true --no-comments"
 #
 
 D=mapbrowser
-$BABEL $D/config.js $D/server.js $D/jscoord.js $D/mapref.js $D/mapbrowser.js $D/widget.js $D/featureinfo.js $D/time.js $D/uiSupport.js $D/popup.js $D/popupmenu.js $D/measure.js $D/toolbar.js $D/areaList.js $D/mousepos.js $D/layerSwitcher.js $D/refSearch.js $D/configSupport.js $D/docreader.js $D/mapInfo.js > mapbrowser-min.js
+$BABEL $D/config.js $D/secUtils.js $D/server.js $D/jscoord.js $D/mapref.js $D/mapbrowser.js $D/widget.js $D/featureinfo.js $D/time.js $D/uiSupport.js $D/popup.js $D/popupmenu.js $D/measure.js $D/toolbar.js $D/areaList.js $D/mousepos.js $D/layerSwitcher.js $D/refSearch.js $D/configSupport.js $D/docreader.js $D/mapInfo.js > mapbrowser-min.js
 
 D=layeredit
 $BABEL $D/layerEdit.js $D/layerList.js $D/wmsLayer.js $D/wfsLayer.js $D/gpxLayer.js $D/drawingLayer.js > layeredit-min.js
@@ -20,7 +20,7 @@ D=featureedit
 $BABEL $D/snow/drawConfig.js $D/snow/drawGlobals.js $D/snow/undoFunctions.js $D/snow/drawStyle.js $D/snow/drawFunctions.js $D/snow/drawTooltipHelper.js $D/snow/drawEvents.js $D/snow/gpxDownload.js $D/snow/drawIcons.js $D/snow/mithrilDrawBox.js $D/snow/mithrilIcons.js $D/snow/measureTooltip.js $D/featureEdit.js $D/properties.js > featureedit-min.js
 
 D=psadmin
-$BABEL $D/passwd.js $D/statusInfo.js $D/syncnodes.js $D/users.js $D/serverconfig.js $D/ownposconfig.js $D/testrest.js > psadmin-min.js
+$BABEL $D/passwd.js $D/statusInfo.js $D/syncnodes.js $D/users.js $D/serverconfig.js $D/ownposconfig.js $D/channels.js $D/testrest.js > psadmin-min.js
 
 #
 # combine and minify css
