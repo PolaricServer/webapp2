d25118bbc1919b9f6f60e740162f679895391cce
Øyvind Hanssen <ohanssen@acm.org>
2025-10-07 12:14:02 +0200

    Fix

--- featureedit/snow/drawGlobals.js
+++ featureedit/snow/drawGlobals.js
@@ -27,6 +27,26 @@ const gpxFormat = ol.format.GPX
 var snow = snow || {};
 
 
+//Set the desired colors underneat with Hex Variables.
+//must contain a string like #fff with values between 0-9 and a-f and 3 or 6 hex decimals
+//Defaults back to default hex if providing wrong values
+//default color: black, default hex: #1f1f1f
+
+snow.color = [
+    {fill: "#1f1f1f", stroke: null}, // Default Black
+    {fill: "#e60505", stroke: "#d00505"}, // Default Red
+    {fill: "#ff9a28", stroke: "#d07000"}, // Default Orange
+    {fill: "#ffff00", stroke: "#a06000"}, // Default Yellow
+    {fill: "#01b301", stroke: "#01a001"}, // default Green
+    {fill: "#05a0d0", stroke: "#0285c0"}, // default Blue
+    {fill: "#a300a3", stroke: null}  // default Purple
+];
+
+
+//Activate Freehand drawing? Y/N
+snow.activateFreedraw = "N"
+
+
 snow.draw = null; 
 snow.snap = false;
 snow.drawSelect = false;
@@ -157,6 +177,5 @@ snow.deactivate = function() {
     // Remove OnClick handler for selecting features.
     snow.drawMap.un('click', snow.handleClick)
 }
-
 
 
